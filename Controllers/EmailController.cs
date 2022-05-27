33fd3a69d868a9a032b2d48536ff12d8b2e5af34
ersin.aydin <ersin.aydin@ORAV02>
2022-05-27 16:58:45 +0300

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using SimpleEmailApp.Services.EmailService;
+using SimpleEmailApp.Models;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+
+        private readonly IEmailService _emailService;    
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+
+            return Ok();
+        }
+    }
+}
